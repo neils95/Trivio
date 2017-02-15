bcfdac122ae755a5bdf7c6654cb8853356916ac7
Neil Sanghrajka <neilsanghrajka@outlook.com>
2017-02-15 05:24:37 -0500

    Migration to Seed database with 10 trivia facts

--- Server/Trivio/Models/Trivia.cs
+++ Server/Trivio/Models/Trivia.cs
@@ -11,11 +11,7 @@ namespace Trivio.Models
 		public string Text { get; set; }
 		public int Upvotes { get; set; }
 		public int Downvotes { get; set; }
-
-		//Foreign key
-		public Category Category { get; set; }
-		public byte CategoryId { get; set; }
-
+		
 		//Foreign key
 		public ICollection<TriviaHistoryModel> UserHistory { get; set; } = new List<TriviaHistoryModel>();
 
