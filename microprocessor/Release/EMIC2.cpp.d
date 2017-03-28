839fc516c1da04f03d8288c5a38c63be2cc80a24
christineklow <clow@bu.edu>
2017-03-28 00:57:23 -0400

    ignore

---
+++ microprocessor/Release/EMIC2.cpp.d
@@ -0,0 +1,24 @@
+C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\EMIC2.cpp.o: \
+ C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\EMIC2.cpp \
+ C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\EMIC2.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/SD.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFat.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2Card.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdInfo.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/FatStructs.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h
---
+++ microprocessor/Release/microprocessor.cpp.d
@@ -0,0 +1,40 @@
+C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\microprocessor.cpp.o: \
+ C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\microprocessor.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/SD.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFat.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2Card.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdInfo.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/FatStructs.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h \
+ C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\I2Cdev.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\MPU6050.h \
+ C:\Users\Christine\AppData\Local\Temp\VMBuilds\microprocessor\uno\Release\EMIC2.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/WiFiEsp.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/WiFiEspClient.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Client.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/WiFiEspServer.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Server.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/WiFiEsp.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/utility/EspDrv.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/utility/RingBuffer.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/utility/RingBuffer.h \
+ C:\Users\Christine\Documents\Arduino\libraries\WiFiEsp\src/utility/debug.h
